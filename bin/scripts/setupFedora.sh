2daa2d9c45a27761f73cdb1376a9e24d4b671874
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-03-15 13:17:41 +0100

    portability

--- bin/scripts/setupFedora.sh
+++ bin/scripts/setupFedora.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 set -e
 
-sudo dnf install -y neovim git tig tmux fish the_silver_searcher docker python
+sudo dnf copr enable dperson/neovim
+sudo dnf install -y neovim python3-neovim git tig tmux fish the_silver_searcher docker python
 sudo chsh -s fish $USER
+
+#Install fzf
+mkdir ~/src
+cd ~/src
+git clone github:junegunn/fzf
+cd fzf
+./install --all
+cd ~
+
+#Setup omf
+curl -L http://get.oh-my.fish | fish
+
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vim/vimrc ~/.config/nvim/init.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cd ~/bin
+git clone https://github.com/phacility/libphutil.git
+git clone https://github.com/phacility/arcanist.git
+cd ~
