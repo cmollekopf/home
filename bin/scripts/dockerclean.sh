cc25af0789f844fe9f7abb15b3620b34acef9346
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-03-15 13:30:14 +0100

    scripts

---
+++ bin/scripts/dockerclean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Removing unused containers"
+docker rm $(docker ps -a -q)
+
+echo "Removing unused images"
+docker rmi $(docker images | grep "^<none>" | awk '{print $3}' ORS=' ')
--- bin/scripts/setupAppleTrackpad.sh
+++ bin/scripts/setupAppleTrackpad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e 'power on\nconnect 88:63:DF:F1:D9:92\nquit' | bluetoothctl
-sleep 1
+sleep 3
 synclient TapButton1=1
 synclient MinSpeed=1.5
 synclient MaxSpeed=3
