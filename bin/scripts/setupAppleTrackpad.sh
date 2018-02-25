5107b3a417fb69507a8fadbf13ed999b0e7f8925
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-02-25 16:02:14 +0100

    Fixed the setupAppleTrackpad script

--- bin/scripts/setupAppleTrackpad.sh
+++ bin/scripts/setupAppleTrackpad.sh
@@ -2,6 +2,6 @@
 rfkill unblock all
 echo -e 'power on\nconnect 88:63:DF:F1:D9:92\nquit' | bluetoothctl
 sleep 3
-synclient TapButton1=1
-synclient MinSpeed=1.5
-synclient MaxSpeed=3
+libinput TapButton1=1
+libinput MinSpeed=1.5
+libinput MaxSpeed=3
