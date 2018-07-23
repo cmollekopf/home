fa6c68dee4c375d0e0c0daeafc666cf5e020e2de
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-07-23 22:27:32 +0200

    Improved setupFedora script

--- bin/scripts/setupFedora.sh
+++ bin/scripts/setupFedora.sh
@@ -5,9 +5,12 @@ sudo dnf copr enable dperson/neovim
 sudo dnf install -y neovim python3-neovim git tig tmux fish the_silver_searcher docker python
 sudo chsh -s fish $USER
 
-#Install fzf
 mkdir ~/src
-cd ~/src
+mkdir ~/bin
+
+
+#Install fzf
+cd ~/bin
 git clone github:junegunn/fzf
 cd fzf
 ./install --all
@@ -21,4 +24,5 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 cd ~/bin
 git clone https://github.com/phacility/libphutil.git
 git clone https://github.com/phacility/arcanist.git
+git clone https://github.com/jwilm/alacritty
 cd ~
