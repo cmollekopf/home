1388120f55bfafd67d22d890944141eb0126e3d4
Christian Mollekopf <mollekopf@kolabsys.com>
2020-01-06 15:13:05 +0100

    Upload script fixes

--- bin/scripts/uploadextension.sh
+++ bin/scripts/uploadextension.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-rsync -avzP $1 $2:/root/
+rsync -avzP $1 root@$2:/root/
 ssh root@$2 "cp /root/$1 /tmp/; plesk bin extension -i /tmp/$1"
--- bin/scripts/uploadlicense.sh
+++ bin/scripts/uploadlicense.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-scp ~/plesk/plesk-tests/data/LicenseFaker.php $1:/usr/local/psa/admin/plib/modules/kolab/library/
+
+set -e
+
+scp ~/src/plesk/plesk-tests/data/LicenseFaker.php root@$1:/usr/local/psa/admin/plib/modules/kolab/library/
+ssh root@$1 "chmod 644 /usr/local/psa/admin/plib/modules/kolab/library/LicenseFaker.php"
+
+scp ~/src/plesk/plesk-tests/data/LicenseFaker-collabora.php root@$1:/usr/local/psa/admin/plib/modules/collabora/library/LicenseFaker.php
+ssh root@$1 "chmod 644 /usr/local/psa/admin/plib/modules/collabora/library/LicenseFaker.php"
