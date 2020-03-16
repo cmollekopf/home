31e28fe939d9bc11f68335e10c72985d3d32b3bb
Christian Mollekopf <mollekopf@kolabsys.com>
2020-03-16 15:06:20 +0100

    Set correct screen for rofi

--- .config/rofi/modi/rofi-boom.sh
+++ .config/rofi/modi/rofi-boom.sh
@@ -2,5 +2,5 @@
 
 shopt -s nullglob globstar
 list="gen"
-selection=$(boom $list | rofi -lines 8 -dmenu -p "boom" | cut -d':' -f1 | sed 's/ *//g')
+selection=$(boom $list | rofi -m $1 -lines 8 -dmenu -p "boom" | cut -d':' -f1 | sed 's/ *//g')
 printf '%s' "$(boom echo $list "$selection")" | head -1 | wl-copy
--- .config/rofi/modi/rofi-pass.sh
+++ .config/rofi/modi/rofi-pass.sh
@@ -5,5 +5,5 @@ prefix=${PASSWORD_STORE_DIR-$HOME/.password-store}
 password_files=( "$prefix"/**/*.gpg )
 password_files=( "${password_files[@]#"$prefix"/}" )
 password_files=( "${password_files[@]%.gpg}" )
-selection=$(printf '%s\n' "${password_files[@]}" | rofi -lines 8 -dmenu -p "gopass")
+selection=$(printf '%s\n' "${password_files[@]}" | rofi -m $1 -lines 8 -dmenu -p "gopass")
 printf '%s' "$(pass "$selection")" | head -1 | wl-copy
