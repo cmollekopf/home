b17449b7119ed04bef842f9e47f7b83d8e3ff67e
Christian Mollekopf <mollekopf@kolabsys.com>
2020-03-16 14:26:13 +0100

    Detect conflicts first on pushlatesttag

--- bin/scripts/pushlatesttag.sh
+++ bin/scripts/pushlatesttag.sh
@@ -2,6 +2,6 @@
 
 LATEST_TAG=`git describe --abbrev=0`
 echo "Pushing latest tag $LATEST_TAG"
-git push origin $LATEST_TAG:$LATEST_TAG
-
+#First push the branch in case of conflicts
 git push
+git push origin $LATEST_TAG:$LATEST_TAG
