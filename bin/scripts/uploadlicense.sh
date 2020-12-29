4f2d95b39011370518ed271669f923488a548c82
Christian Mollekopf <mollekopf@kolabsys.com>
2020-12-29 19:22:56 +0100

    Adjusted default paths

--- bin/scripts/uploadlicense.sh
+++ bin/scripts/uploadlicense.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-scp ~/src/plesk/plesk-tests/data/LicenseFaker.php root@$1:/usr/local/psa/admin/plib/modules/kolab/library/
+scp ~/src/plesk-tests/data/LicenseFaker.php root@$1:/usr/local/psa/admin/plib/modules/kolab/library/
 ssh root@$1 "chmod 644 /usr/local/psa/admin/plib/modules/kolab/library/LicenseFaker.php"
 
-scp ~/src/plesk/plesk-tests/data/LicenseFaker-collabora.php root@$1:/usr/local/psa/admin/plib/modules/collabora/library/LicenseFaker.php
+scp ~/src/plesk-tests/data/LicenseFaker-collabora.php root@$1:/usr/local/psa/admin/plib/modules/collabora/library/LicenseFaker.php
 ssh root@$1 "chmod 644 /usr/local/psa/admin/plib/modules/collabora/library/LicenseFaker.php"
