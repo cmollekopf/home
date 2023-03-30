af1e83aaa3152d5dd605871842a759b5d33ab464
Christian Mollekopf <mollekopf@apheleia-it.ch>
2023-03-30 08:50:53 +0200

    imapcli

--- bin/scripts/imapcli.rb
+++ bin/scripts/imapcli.rb
@@ -3,22 +3,44 @@
 require "net/imap"
 require 'thor'
 
+class IMAP < Net::IMAP
+
+    def getmetadata(mailbox, entry)
+      synchronize do
+        send_command("GETMETADATA", mailbox, entry)
+        return @responses.delete("METADATA")[-1]
+      end
+    end
+
+    def setmetadata(mailbox, entry, value)
+      # send_command("SETMETADATA", mailbox, [entry, IMAP.encode_utf7(value)])
+      # FIXME I can't make this to send the value quoted.
+      # The implementation does not support it: https://github.com/ruby/net-imap/blob/e5bcb677d0cb11c2de38dd5a67fca3de3a90d5fb/lib/net/imap/command_data.rb#L54
+      send_command("SETMETADATA", mailbox, [entry, value])
+    end
+end
+
+
 class ImapCli < Thor
   class_option :host
   class_option :port
   class_option :username
   class_option :password
   class_option :ssl, :type => :boolean
+  class_option :debug, :type => :boolean
 
   no_commands {
     def imap()
       if !@imap
         if options[:ssl]
-          @imap = Net::IMAP.new(options[:host], :port => options[:port], :ssl => {
+          @imap = IMAP.new(options[:host], :port => options[:port], :ssl => {
             :verify_mode => OpenSSL::SSL::VERIFY_NONE
           })
         else
-          @imap = Net::IMAP.new(options[:host], :port => options[:port], :ssl => false)
+          @imap = IMAP.new(options[:host], :port => options[:port], :ssl => false)
+        end
+        if options[:debug]
+          IMAP.debug = true
         end
         @imap.login(options[:username], options[:password])
       end
@@ -36,9 +58,39 @@ class ImapCli < Thor
     p imap.list("", "**")
   end
 
+  desc "namespace", "Namespace."
+  def namespace()
+    p imap.namespace()
+  end
+
+  desc "capability", "Capability."
+  def capability()
+    p imap.capability()
+  end
+
   desc "select", "Select."
   def select(folder)
     p imap.select(folder)
+  end
+
+  desc "create", "Create."
+  def create(folder)
+    p imap.create(folder)
+  end
+
+  desc "getmetadata", "Getmetadata."
+  def getmetadata(folder, entry)
+    p imap.getmetadata(folder, entry)
+  end
+
+  desc "getacl", "getacl."
+  def getacl(folder)
+    p imap.getacl(folder)
+  end
+
+  desc "setmetadata", "Setmetadata."
+  def setmetadata(folder, entry, value)
+    p imap.setmetadata(folder, entry, value)
   end
 
   desc "download", "Download."
